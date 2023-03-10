d466a05d237b17937c627c3460ae6a6512ab7eae
Pablo Marián <pablomarianv@gmail.com>
2023-03-10 18:46:49 +0100

    Arreglos visuales DOM2

--- js/BarcelonaCampeonDOM.js
+++ js/BarcelonaCampeonDOM.js
@@ -1,8 +1,8 @@
 //console.log(document.querySelector("tr"))
-console.log(document.getElementsByTagName("td"))
+//console.log(document.getElementsByTagName("td"))
 
 //Parte 1
-let filasTabla = document.getElementsByTagName("tr")
+let filasTabla = document.querySelectorAll("tr")
 filasTabla[1].setAttribute("style","background-color: yellow;")
 
 //Parte 2
@@ -19,21 +19,58 @@ contenedores[1].setAttribute("style", "border-color: black;" +
 
 //Parte 5
 let segundones = document.getElementsByTagName("span")
-let huecos = document.getElementsByTagName("td")
+let columnas = filasTabla[2].getElementsByTagName("td")
 
-huecos[6].textContent = segundones[0].textContent
-huecos[7].textContent = segundones[1].textContent
+columnas[2].textContent = segundones[0].textContent
+columnas[3].textContent = segundones[1].textContent
 
 //Parte 6
 pepes[1].outerHTML = ""
 
 //DOM-2
+columnas = filasTabla[0].getElementsByTagName("th")
+for (let i = 0; i<(columnas.length);i++) {
+    var acomodador
+    var perchero
+    if (i){
+        acomodador = perchero
+        perchero = columnas[i].textContent
+        columnas[i].textContent = acomodador
+
+    }else {
+        perchero = columnas[i].textContent
+        columnas[i].textContent = "Escudo"
+    }
+}
+
 //Parte 1
-huecos[9].textContent = "3"
-huecos[13].textContent = "4"
-huecos[17].textContent = "5"
+for (let i = 1; i < filasTabla.length; i++) {
+    columnas = filasTabla[i].getElementsByTagName("td")
+    columnas[1].textContent = `${i}`
+}
+/*let wrapper = document.getElementsByTagName("img")[0]
+columnas = filasTabla[1].getElementsByTagName("td")
+columnas[1].appendChild(wrapper)*/
 
 //Parte 2
+let ultimos5 = [
+    [1,-1,1,1,1],
+    [0,0,1,1,-1],
+    [1,0,1,1,0],
+    [0,-1,0,1,-1],
+    [0,1,1,1,-1]
+]
 
+for (let i = 0; i < filasTabla.length; i++) {
+    columnas = filasTabla[i].getElementsByTagName("td")
+    if (i) {
+
+    }else {
+
+        //filasTabla[i].appendChild()
+    }
+
+
+}
 
 
